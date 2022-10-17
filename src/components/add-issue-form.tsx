8384029aa48bf00840db55f062bca82e94eb9bfc
JlHen <reactlearningman@gmail.com>
2022-10-17 18:52:44 -0500

    A more fully fleshed out form

--- src/components/add-issue-form.tsx
+++ src/components/add-issue-form.tsx
@@ -1,12 +1,19 @@
 import Issue from "../models/Issue";
+import {useState} from "react";
 
 interface Props {
     addNewIssue: (newIssue: Issue)=>void
 }
 
 function AddIssueForm() {
+    const [description, setDescription] = useState("")
+    const [assignee, setAssignee] = useState("")
     return (
         <form className="AddIssueForm">
+            <label htmlFor="description">Description</label>
+            <input type="text" name="description" id="description" value={description} onChange={(changeEvent)=>setDescription(changeEvent.target.value)}/>
+            <label htmlFor="assignee">Assignee</label>
+            <input type="text" name="assignee" id="assignee" value={assignee} onChange={(changeEvent)=>setAssignee(changeEvent.target.value)}/>
         </form>
     )
 }
