6cc9774476881b4092ecb8d2da2300aab3596724
Ethan Cloin <psnethan@gmail.com>
2022-10-17 21:32:50 -0400

    service to add issue works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import AddIssueForm from "./components/add-issue-form";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
--- src/services/IssueService.ts
+++ src/services/IssueService.ts
@@ -1,14 +1,28 @@
 import axios from "axios";
-import Issue from "../models/Issue";
+import Issue, { IssueMetadata } from "../models/Issue";
 
 const dbApiKey = process.env.REACT_APP_DB_API_KEY_DEV || "";
+const issuesDocumentUrl = "https://issuetracker-b807.restdb.io/rest/issue";
 
-export const getAllIssues = (): Promise<Issue> => {
+export const getAllIssues = (): Promise<Issue[]> => {
   return axios
-    .get("https://issuetracker-b807.restdb.io/rest/issue", {
+    .get(issuesDocumentUrl, {
       headers: {
         "x-api-key": dbApiKey,
       },
     })
     .then((res) => res.data);
+};
+
+export const addIssue = (newIssue: Issue): Promise<IssueMetadata> => {
+  return axios({
+    method: "post",
+    url: issuesDocumentUrl,
+    data: {
+      assignee: newIssue.assignee,
+      description: newIssue.description,
+      status: newIssue.status,
+    },
+    headers: { "x-api-key": dbApiKey },
+  }).then((res) => res.data);
 };
