fb37302543a91bbbff479cd6390450def035ebb6
Ethan Cloin <psnethan@gmail.com>
2022-10-17 21:58:09 -0400

    remove id from Issue payload

--- src/components/add-issue-form.tsx
+++ src/components/add-issue-form.tsx
@@ -5,24 +5,46 @@ interface Props {
   addNewIssue: (newIssue: Issue) => void;
 }
 
-function AddIssueForm({addNewIssue}:Props) {
-    const [givenDescription, setGivenDescription] = useState("")
-    const [givenAssignee, setGivenAssignee] = useState("")
-    function handleSubmission(submitEvent: FormEvent) {
-        submitEvent.preventDefault()
-        const newId: number = Math.round(Math.random())
-        addNewIssue({id: newId.toString(), assignee:givenAssignee, description:givenDescription, status:"open"})
-    }
-    return (
-        <form className="AddIssueForm" onSubmit={(submissionEvent)=>{handleSubmission(submissionEvent)}}>
-            <h2>Add New Issue</h2>
-            <label htmlFor="description">Description</label>
-            <input type="text" name="description" id="description" value={givenDescription} onChange={(changeEvent)=>setGivenDescription(changeEvent.target.value)}/>
-            <label htmlFor="assignee">Assignee</label>
-            <input type="text" name="assignee" id="assignee" value={givenAssignee} onChange={(changeEvent)=>setGivenAssignee(changeEvent.target.value)}/>
-            <input type="submit" value="Add New Issue"/>
-        </form>
-    )
+function AddIssueForm({ addNewIssue }: Props) {
+  const [givenDescription, setGivenDescription] = useState("");
+  const [givenAssignee, setGivenAssignee] = useState("");
+  function handleSubmission(submitEvent: FormEvent) {
+    submitEvent.preventDefault();
+    addNewIssue({
+      assignee: givenAssignee,
+      description: givenDescription,
+      status: "open",
+    });
+  }
+  return (
+    <form
+      className="AddIssueForm"
+      onSubmit={(submissionEvent) => {
+        handleSubmission(submissionEvent);
+      }}
+    >
+      <h2>Add New Issue</h2>
+      <label htmlFor="description">Description</label>
+      <input
+        type="text"
+        name="description"
+        id="description"
+        value={givenDescription}
+        onChange={(changeEvent) =>
+          setGivenDescription(changeEvent.target.value)
+        }
+      />
+      <label htmlFor="assignee">Assignee</label>
+      <input
+        type="text"
+        name="assignee"
+        id="assignee"
+        value={givenAssignee}
+        onChange={(changeEvent) => setGivenAssignee(changeEvent.target.value)}
+      />
+      <input type="submit" value="Add New Issue" />
+    </form>
+  );
 }
 
 export default AddIssueForm;
