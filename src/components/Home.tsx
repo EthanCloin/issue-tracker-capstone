264d5c74f2000d88351a03751d1aee6cf2d5e0b9
JlHen <reactlearningman@gmail.com>
2022-10-19 18:44:00 -0500

    Merge with main

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,10 +4,16 @@ import { addIssue, getAllIssues } from "../services/IssueService";
 import AddIssueForm from "./add-issue-form";
 import "./Home.css";
 import IssueList from "./issue-list";
+import {useSearchParams} from "react-router-dom";
+import {stat} from "fs";
 
 const Home = () => {
-  const [issues, setIssues] = useState<IssueResponse[]>([]);
-
+  const [issues, setIssues] = useState<Issue[]>([]);
+  const [searchParameters] = useSearchParams()
+  const assigneeFilter = searchParameters.get("assignee")
+  const statusFilter = searchParameters.get("status")
+  console.log("assignee: " + assigneeFilter)
+  console.log("status: " + statusFilter)
   useEffect(() => {
     // this only runs when the home component is first mounted
     // adding issues to the dependency array makes it keep calling
@@ -22,10 +28,18 @@ const Home = () => {
     console.log("add response: ", rsp);
     getAllIssues().then((res) => setIssues(res));
   };
+  let filteredIssues:Issue[] = []
+  issues.forEach((issue)=>filteredIssues.push(issue))
+  if (assigneeFilter) {
+    filteredIssues.filter((issue)=>issue.assignee === assigneeFilter)
+  }
+  if (statusFilter) {
+    filteredIssues.filter((issue)=>issue.status === statusFilter)
+  }
   return (
     <div className="Home">
       <AddIssueForm addNewIssue={addNewIssue} />
-      <IssueList issues={issues}/>
+      <IssueList issues={filteredIssues}/>
     </div>
   );
 };
