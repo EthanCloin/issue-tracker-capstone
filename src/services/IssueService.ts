fc27fbdcb38b9952fb132f23c69d9091a9766b09
Ethan Cloin <psnethan@gmail.com>
2022-10-20 13:31:42 -0400

    implement getIssue service (untested)

--- src/services/IssueService.ts
+++ src/services/IssueService.ts
@@ -14,6 +14,16 @@ export const getAllIssues = (): Promise<IssueResponse[]> => {
     .then((res) => res.data);
 };
 
+export const getIssue = (id: string): Promise<IssueMetadata> => {
+  return axios
+    .get(`${issuesDocumentUrl}/${id} `, {
+      headers: {
+        "x-api-key": dbApiKey,
+      },
+    })
+    .then((res) => res.data);
+};
+
 export const addIssueToDb = (newIssue: Issue): Promise<IssueResponse> => {
   return axios({
     method: "post",
