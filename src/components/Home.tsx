a60e2fea7512ed1fca8ca3c1b7e5793a888684e3
Ethan Cloin <psnethan@gmail.com>
2022-10-19 20:33:25 -0400

    prettier formatting

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,15 +4,15 @@ import { addIssue, getAllIssues } from "../services/IssueService";
 import AddIssueForm from "./add-issue-form";
 import "./Home.css";
 import IssueList from "./issue-list";
-import {useSearchParams} from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [issues, setIssues] = useState<IssueResponse[]>([]);
-  const [searchParameters] = useSearchParams()
-  const assigneeFilter = searchParameters.get("assignee")
-  const statusFilter = searchParameters.get("status")
-  console.log("assigneeFilter: " + assigneeFilter)
-  console.log("statusFilter: " + statusFilter)
+  const [searchParameters] = useSearchParams();
+  const assigneeFilter = searchParameters.get("assignee");
+  const statusFilter = searchParameters.get("status");
+  console.log("assigneeFilter: " + assigneeFilter);
+  console.log("statusFilter: " + statusFilter);
   useEffect(() => {
     // this only runs when the home component is first mounted
     // adding issues to the dependency array makes it keep calling
@@ -27,21 +27,21 @@ const Home = () => {
     console.log("add response: ", rsp);
     getAllIssues().then((res) => setIssues(res));
   };
-  let filteredIssues:IssueResponse[] = []
-  issues.forEach((issue)=>{
+  const filteredIssues: IssueResponse[] = [];
+  issues.forEach((issue) => {
     if (assigneeFilter && issue.assignee !== assigneeFilter) {
-      return
+      return;
     }
     if (statusFilter && issue.status !== statusFilter) {
-      return
+      return;
     }
-    filteredIssues.push(issue)
-  })
-  console.log("filtered issues: ", filteredIssues)
+    filteredIssues.push(issue);
+  });
+  console.log("filtered issues: ", filteredIssues);
   return (
     <div className="Home">
       <AddIssueForm addNewIssue={addNewIssue} />
-      <IssueList issues={filteredIssues}/>
+      <IssueList issues={filteredIssues} />
     </div>
   );
 };
