76235604c2d2669bda2d5e43bb9fceee91d8a6a7
Ethan Cloin <psnethan@gmail.com>
2022-10-23 17:42:58 -0400

    nice header w link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,35 @@
 import { useContext } from "react";
+import { useTheme } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
 import { IssueResponse } from "../models/Issue";
 import "./Header.css";
 import IssuesContext from "../context/IssueContext";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   issues: IssueResponse[];
 }
 
 const Header = () => {
-  const openCount = [];
-  const { addIssue, issues } = useContext(IssuesContext);
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const { issues } = useContext(IssuesContext);
 
-  issues.forEach((issue) => {
-    if (issue.status === "open") {
-      openCount.push({ issue });
-    }
-  });
+  const openCount = issues.filter((issue) => issue.status == "open").length;
+  const openPercent = ((openCount / issues.length) * 100).toFixed(2);
 
   return (
-    <div className="Header">
-      <h1>** Welcome to the Issues Board **</h1>
-      <h2>
-        Current Total Issue Count: {issues.length}{" "}
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Current Open Issue
-        Percentage: {((openCount.length / issues.length) * 100).toFixed(2)}
-      </h2>
-    </div>
+    <AppBar className="Header" position="sticky">
+      <Toolbar className="inner-header">
+        <h1 onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
+          Issue Tracker
+        </h1>
+        <section className="issue-stats">
+          <p className="issue-count">Issues: {issues.length}</p>
+          <p className="open-percent">Open: {openPercent}%</p>
+        </section>
+      </Toolbar>
+    </AppBar>
   );
 };
 
