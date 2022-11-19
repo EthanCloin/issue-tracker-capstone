9107a99c002b9614538da33fd78e1467ed455c5a
Ethan Cloin <psnethan@gmail.com>
2022-11-19 15:54:50 -0500

    include new ip for hosted backend

--- src/services/IssueService.ts
+++ src/services/IssueService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IssuePrototype, Issue } from "../models/Issue";
 
-const backendDevURL = "http://0.0.0.0:8000";
+const backendDevURL = "http://24.199.84.215:8000";
 
 export const getAllIssues = (
   offset?: number,
